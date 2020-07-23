199f6ef52c19ddef256ee26b75599639fdc034bb
Zhangyao719 <1258858403@qq.com>
2020-07-23 16:14:07 +0800

    NavBar/NavFooter/ServiceBar/swiper/轮播侧边栏

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="index">
     <div class="container">
-      <div class="nav-menu">
-        <ul class="menu-wrap">
-          <li class="menu-item">
-            <a href="javascript:;">手机 电话卡</a>
-            <div class=""></div>children
-          </li>
-          <li class="menu-item">
-            <a href="javascript:;">手机 电话卡</a>
-          </li>
-        </ul>
-      </div>
-      <div class="ads-box">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <!-- 列表的嵌套 -->
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="sub in item" :key="sub.id">
+                    <a :href="'/#/product/'+sub.id">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'">
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插件版</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="item in slideList" :key="item.id">
             <a :href="/#/+item.id"><img :src="item.img"></a>
@@ -72,6 +100,36 @@ export default {
           id: '46',
           img: '/imgs/slider/slide-3.jpg'
         }
+      ],
+      // 2维数组 显示侧边栏的每一栏具体内容（4*6）
+      menuList: [
+        [
+          {
+            id: 30,
+            img: 'imgs/item-box-1.png',
+            name: '小米cc9'
+          },
+          {
+            id: 31,
+            img: 'imgs/item-box-2.png',
+            name: '小米8青春版'
+          },
+          {
+            id: 32,
+            img: 'imgs/item-box-3.jpg',
+            name: 'Redmi K20 Pro'
+          },
+          {
+            id: 33,
+            img: 'imgs/item-box-4.jpg',
+            name: '移动4G专区'
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
       ]
     }
   }
@@ -79,10 +137,85 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  @import '../styles/mixin.less';
   .index {
-    .ads-box {
+    .swiper-box {
+      position: relative;
+      .nav-menu {
+        position: absolute;
+        width: 264px;
+        height: 451px;
+        z-index: 9;
+        padding: 26px 0;
+        // background-color: #55585a7a;
+        background: rgba(67,68,71, .7);
+        .menu-wrap {
+          .menu-item {
+            height: 50px;
+            line-height: 50px;
+            > a {
+              position: relative;
+              display: block;
+              font-size: 16px;
+              color: #fff;
+              padding-left: 30px;
+              &:after {
+                position: absolute;
+                right: 30px;
+                top: 17.5px;
+                content: '';
+                .mixin_bgImg(10px,15px,'/imgs/icon-arrow.png');
+              }
+              &:hover {
+                background-color: #FF6600;
+                + .children {
+                  display: block;
+                }
+              }
+            }
+            .children {
+              display: none;
+              width: 962px;
+              height: 451px;
+              background-color: #fff;
+              position: absolute;
+              top: 0;
+              left: 264px;
+              border: 1px solid #e5e5e5;
+              ul {
+                display: flex;
+                justify-content: space-between;
+                height: 75px;
+                padding-left: 30px;
+
+                li {
+                  flex: 1;
+                  height: 75px;
+                  line-height: 75px;
+                  a {
+                    font-size: 14px;
+                    color: #333;
+                    &:hover {
+                      color: #FF6600;
+                    }
+                  }
+                  img {
+                    width: 42px;
+                    height: 35px;
+                    vertical-align: middle;
+                    margin-right: 15px;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
       .swiper-container {
         height: 451px;
+        .swiper-button-prev {
+          left: 274px;
+        }
         img {
           width: 100%;
           height: 100%;
